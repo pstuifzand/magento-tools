9f703c15399e9a30910a81ce53b5815d2c1d8b2f
Peter Stuifzand <peter@stuifzand.eu>
2015-03-01 19:35:04 +0100

    Cleanup of magento-config.pl

--- magento-config.pl
+++ magento-config.pl
@@ -1,15 +1,44 @@
 #!/usr/bin/env perl
 use feature "say";
+use IPC::Open3;
+use Data::Dumper;
 
-open my $check_fh, '|-', '/home/peter/work/magento-tools/magento-config `find . -type f -name \'system.xml\'`'
-    or die "Can't open: $!";
+my @files = `find . -type f -name \'system.xml\'`;
+for (@files) { chomp; }
+my @cmd = ('/home/peter/work/magento-tools/magento-config', @files);
 
-while (<ARGV>) {
-    chomp;
-    if (m/getStoreConfig(?:Flag)?\('([^']+)'\)/) {
-        print {$check_fh} "$1\n";
+my ($in, $out, $err);
+my $pid = open3($in, $out, $err, @cmd);
+
+my $show_filename = 1;
+sub check {
+    my ($in, $out, $filename, $key) = @_;
+    print {$in} "$1\n";
+    my $output = <$out>;
+    if ($output =~ m/^not found/) {
+        if ($show_filename) {
+            say "$filename";
+            $show_filename = 0;
+        }
+        print $output;
     }
 }
 
-close $check_fh;
+for my $filename (@ARGV) {
+    open my $fh, '<', $filename or die "Can't open $filename: $!";
+    $show_filename = 1;
+    while (<$fh>) {
+        chomp;
+        if (m/const\s+XML_PATH_[A-Z_]+\s+=\s+'([^']+)';/) {
+            check($in, $out, $filename, $1);
+        } elsif (m/getStoreConfig(?:Flag)?\('([^']+)'\)/) {
+            check($in, $out, $filename, $1);
+        } elsif (m/getStoreConfig(?:Flag)?\("([^"]+)"\)/) {
+            check($in, $out, $filename, $1);
+        }
+    }
+}
+close $in;
+close $out;
 
+waitpid $pid, 0;
