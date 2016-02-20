afcb16e3ffa57b896c7dd6164bfd1e37cbf1f61f
Peter Stuifzand <peter@stuifzand.eu>
2016-02-20 14:13:58 +0100

    Fix the directory names of the created model

--- model-create.pl
+++ model-create.pl
@@ -5,6 +5,12 @@ my $codepool = 'community';
 my $module_name = shift or die "No module_name";
 my $model_name  = shift or die "No model_name";
 
+my $module_filename = $module_name;
+$module_filename =~ s{_}{/}g;
+
+my $model_filename = $model_name;
+$model_filename =~ s{_}{/}g;
+
 my $basedir = "xml/test/app/code/$codepool";
 
 my $uri = lc($module_name) . '/' . lc($model_name);
@@ -14,19 +20,19 @@ my @parts = (
         class_name => "${module_name}_Model_${model_name}",
         template   => 'tt/model-model.tt',
         model_uri  => $uri,
-        filename   => $basedir . "/${module_name}/Model/${model_name}.php",
+        filename   => $basedir . "/${module_filename}/Model/${model_filename}.php",
     },
     {
         class_name => "${module_name}_Model_Resource_${model_name}",
         template   => 'tt/model-resource.tt',
         model_uri  => $uri,
-        filename   => $basedir . "/${module_name}/Model/Resource/${model_name}.php",
+        filename   => $basedir . "/${module_filename}/Model/Resource/${model_filename}.php",
     },
     {
         class_name => "${module_name}_Model_Resource_${model_name}_Collection",
         template   => 'tt/model-collection.tt',
         model_uri  => $uri,
-        filename   => $basedir . "/${module_name}/Model/Resource/${model_name}/Collection.php",
+        filename   => $basedir . "/${module_filename}/Model/Resource/${model_filename}/Collection.php",
     }
 );
 
