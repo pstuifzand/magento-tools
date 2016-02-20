52d2d858d17cf8fb181929594f60c123fac92780
Peter Stuifzand <peter@stuifzand.eu>
2016-02-20 11:12:05 +0100

    Add script for create Model hierarchies

---
+++ model-create.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+use Template;
+
+my $codepool = 'community';
+my $module_name = shift or die "No module_name";
+my $model_name  = shift or die "No model_name";
+
+my $basedir = "xml/test/app/code/$codepool";
+
+my $uri = lc($module_name) . '/' . lc($model_name);
+
+my @parts = (
+    {
+        class_name => "${module_name}_Model_${model_name}",
+        template   => 'tt/model-model.tt',
+        model_uri  => $uri,
+        filename   => $basedir . "/${module_name}/Model/${model_name}.php",
+    },
+    {
+        class_name => "${module_name}_Model_Resource_${model_name}",
+        template   => 'tt/model-resource.tt',
+        model_uri  => $uri,
+        filename   => $basedir . "/${module_name}/Model/Resource/${model_name}.php",
+    },
+    {
+        class_name => "${module_name}_Model_Resource_${model_name}_Collection",
+        template   => 'tt/model-collection.tt',
+        model_uri  => $uri,
+        filename   => $basedir . "/${module_name}/Model/Resource/${model_name}/Collection.php",
+    }
+);
+
+my $tt = Template->new();
+
+for my $part (@parts) {
+    $tt->process($part->{template}, $part, $part->{filename}) or die $tt->error;
+}
+
