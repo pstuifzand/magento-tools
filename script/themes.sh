6398f28ffc23949a2fd6c2837647a8fac7f9efdb
Peter Stuifzand <peter@stuifzand.eu>
2016-02-22 18:28:24 +0100

    Cleanup of themes.sh

--- script/themes.sh
+++ script/themes.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /var/www/html/magento/
-find app/design/frontend -mindepth 2 -maxdepth 2 -type d | sed 's/app\/design\/frontend\///'
+find app/design/frontend -mindepth 2 -maxdepth 2 -type d | sed 's#app/design/frontend/##'
