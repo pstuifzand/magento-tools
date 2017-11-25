20916cf160ce59ced813c626919f2254fa1f9628
Peter Stuifzand <peter@stuifzand.eu>
2017-11-25 19:47:12 +0100

    Improve version scripts

--- script/current-version.sh
+++ script/current-version.sh
@@ -3,5 +3,4 @@ root=`git rev-parse --show-toplevel`
 module=${1:-`current-module`}
 dir=`echo $module | sed -e 's#_#/#'`
 version=`xmlstarlet sel -t -v /config/modules/$module/version  $root/app/code/community/$dir/etc/config.xml`
-next_version=`echo $version | perl -pE '($a,$b,$c)=m{(\d+)\.(\d+)\.(\d+)}; $b++; $_="$a.$b.$c"'`
-echo $next_version
+echo $version
