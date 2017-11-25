97df05725818e13ef587eaaad26934c4819b65d6
Peter Stuifzand <peter@stuifzand.eu>
2017-11-25 19:40:59 +0100

    Skip block in action, and show block on one line

--- src/block-lister.cpp
+++ src/block-lister.cpp
@@ -9,33 +9,49 @@
 
 struct user_data {
     char* filename;
+    int in_action;
+};
+
+struct Block {
+    std::string type;
+    std::string name;
+    std::string tmpl;
+    std::string as;
 };
 
 void root_start_element_handler(void* udata, const XML_Char* name, const XML_Char** atts)
 {
     user_data* data = (user_data*)udata;
-    if (strcmp(name, "block") == 0) {
+
+    if (strcmp(name, "action") == 0) {
+        data->in_action = 1;
+    }
+    else if (strcmp(name, "block") == 0) {
+        if (data->in_action) return;
+
         bool type_found = false;
         bool name_found = false;
+
+        Block block;
 
         while (*atts) {
             if (strcmp(*atts, "type") == 0) {
                 ++atts;
-                std::cout << "type: " << *atts << "\n";
+                block.type = *atts;
                 type_found = true;
             }
             else if (strcmp(*atts, "name") == 0) {
                 ++atts;
-                std::cout << "name: " << *atts << "\n";
+                block.name = *atts;
                 name_found = true;
             }
             else if (strcmp(*atts, "template") == 0) {
                 ++atts;
-                std::cout << "template: " << *atts << "\n";
+                block.tmpl = *atts;
             }
             else if (strcmp(*atts, "as") == 0) {
                 ++atts;
-                std::cout << "as: " << *atts << "\n";
+                block.as = *atts;
             }
             else {
                 ++atts;
@@ -44,9 +60,12 @@ void root_start_element_handler(void* udata, const XML_Char* name, const XML_Cha
             ++atts;
         }
 
+        std::cout << block.type << " " << block.name << " " << block.as << " " << block.tmpl << "\n";
+
         if (!name_found) {
             std::cerr << data->filename << ": No name found in block\n";
         }
+
         if (!type_found) {
             std::cerr << data->filename << ": No type found in block\n";
         }
@@ -55,6 +74,10 @@ void root_start_element_handler(void* udata, const XML_Char* name, const XML_Cha
 
 void root_end_element_handler(void* udata, const XML_Char* name)
 {
+    if (strcmp(name, "action") == 0) {
+        user_data* data = (user_data*)udata;
+        data->in_action = 0;
+    }
 }
 
 void character_data_handler(void* udata, const XML_Char* s, int len)
@@ -90,6 +113,7 @@ int main(int argc, char** argv)
 
     while (argc) {
         user_data data;
+        data.in_action = 0;
         data.filename = *argv;
 
         XML_SetUserData(parser.handle(), &data);
